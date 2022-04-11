6e66811c515707d5d395d49ca403b4f4c1d1f267
Maxim Vavilkin <maxim00066@gmail.com>
2022-04-11 21:38:13 +0300

    fix: fixed styles3

--- src/components/misc/Layouts.js
+++ src/components/misc/Layouts.js
@@ -1,4 +1,5 @@
 import tw from "twin.macro";
+import { css } from "styled-components/macro"; //eslint-disable-line
 
 export const Container = tw.div`relative`;
 export const ContentWithPaddingXl= tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
